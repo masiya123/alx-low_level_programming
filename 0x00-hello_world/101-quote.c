d3c70df99871ded55031046fbab36b4943c480e8
siyabong <siyabongakhumalo602@gmail.com>
2023-07-13 05:36:57 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints "and that piecs of art of art is useful" - Dora korper, 2015-10-19",
+ * following by a newline, to standard error
+ * Return: Always 1,
+ */
+
+int main(void)
+{
+    write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",59);
+    return (1);    
