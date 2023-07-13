a5aabdd75dd26ad878a1421393c3a70fa5ebf196
siyabong <siyabongakhumalo602@gmail.com>
2023-07-13 11:35:45 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 /**
- * main - prints "and that piecs of art is useful"
- * - Dora korper, 2015-10-19",
+ * main - prints and that piecs of art is useful Dora korper
  * Return: Always 1,
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
