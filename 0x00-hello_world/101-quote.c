aa39a5062a59ac818a6bcb457139e5d801d29310
siyabong <siyabongakhumalo602@gmail.com>
2023-07-13 11:29:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,8 @@
  * - Dora korper, 2015-10-19",
  * Return: Always 1,
  */
-
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
-    return (1);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
 }
