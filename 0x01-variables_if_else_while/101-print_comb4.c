b33d33cf98a461d62de26819ed91512bb6dc5372
siyabong <siyabongakhumalo602@gmail.com>
2023-07-14 10:46:27 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ *  main - p prints all possible different combination of those of digits
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+	int hundreds = '0';
+
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = '0'; ones <= '9' tens ++)
+		{
+			for (ones = '0'; ones <= '9'; ones++)
+			{
+				if(!((ones == tens) ||(tens == hundreds) || (tens > ones) || (hundreds > tens)))
+				{
+					putchar(hundreds);
+					putchar(tens);
+					putchar(one);
+					if (!(ones == '9' && hundreds == '7' && tens == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+}
+
+putchar('\n');
+return (0);
+
