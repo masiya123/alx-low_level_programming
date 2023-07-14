00fb5b52622eccc73b340667ac17c41895b5c2ea
siyabong <siyabongakhumalo602@gmail.com>
2023-07-14 12:07:34 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * main - prog Entry point
+ * main - prog Entry point 
  * Return: Always 0 (Success)
  */
+
 int main(void)
 	{
 	int a;
-	
-	for (a = 0; a < 10; a++)
+
+	for (a = 0; < 10; a++)
 	{
 		putchar(a + '0');
 		if (a < 9)
@@ -16,7 +17,6 @@ int main(void)
 			putchar(' ');
 		}
 	}
-
-	putchar('\n')
+	putchar('\n');
 	return (0);
 	}
