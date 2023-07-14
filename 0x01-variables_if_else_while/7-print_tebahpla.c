0f9314a882c9e26b97322e42fd1f5d609e8b0c73
siyabong <siyabongakhumalo602@gmail.com>
2023-07-14 06:23:12 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  main - prog Entry point
+ *  Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
