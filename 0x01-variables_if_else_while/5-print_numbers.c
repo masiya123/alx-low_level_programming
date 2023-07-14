f5bc9e72e5fb1b46216e510936fc7658cea4a3fa
siyabong <siyabongakhumalo602@gmail.com>
2023-07-14 05:56:16 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  main - Prog  Entry point
+ *  Return: Alway 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
