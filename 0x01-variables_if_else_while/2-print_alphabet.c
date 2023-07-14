a60bca5329012a17866c9799919efba4364cc983
siyabong <siyabongakhumalo602@gmail.com>
2023-07-14 04:35:35 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Alway 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
