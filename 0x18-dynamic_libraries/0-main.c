9477229ecc06e9fb223d94d15237483fa4270b19
siyabonga <siyabongakhumalo602@gmail.com>
2024-01-30 07:49:31 -0800

    libdynamic.so

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
+}
---
+++ 0x18-dynamic_libraries/strlen.c
@@ -0,0 +1,6 @@
+#include <string.h>
+
+size_t _strlen(const char *str) {
+    return strlen(str);
+}
+
