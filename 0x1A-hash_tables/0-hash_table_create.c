2f3f79187ebd65c2e3cdd5e8336789889cbe5745
siyabonga <siyabongakhumalo602@gmail.com>
2024-02-16 02:52:26 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,52 +1,25 @@
-#include <stdlib.h>
-#include <string.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: the size of the array
+ * hash_table_create - Creates a hash table.
+ * @size: Size of the array in the hash table
  *
- * Return: a pointer to the newly created hash table, or NULL if something went wrong
+ * Return: Pointer to the newly created hash table, or NULL on failure
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
+    hash_table_t *ht = malloc(sizeof(hash_table_t));
 
-    ht = malloc(sizeof(hash_table_t));
     if (!ht)
         return (NULL);
+
     ht->size = size;
     ht->array = calloc(size, sizeof(hash_node_t *));
+
     if (!ht->array)
     {
         free(ht);
         return (NULL);
     }
     return (ht);
-}
-
-/**
- * hash_table_destroy - frees the memory allocated for a hash table
- * @ht: a pointer to the hash table to be destroyed
- */
-void hash_table_destroy(hash_table_t *ht)
-{
-    unsigned long int i;
-
-    for (i = 0; i < ht->size; i++)
-    {
-        hash_node_t *node = ht->array[i];
-        hash_node_t *next;
-
-        while (node)
-        {
-            next = node->next;
-            free(node->key);
-            free(node->value);
-            free(node);
-            node = next;
-        }
-    }
-    free(ht->array);
-    free(ht);
 }
